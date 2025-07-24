117ad0d4eb8522741c2955bad1b24a837456b134
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2025-07-25 01:00:15 +0300

    Rewrite to async fs I/O operatins

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
 'use strict';
 
-const path = require('node:path');
 const { Blockchain } = require('./lib/storage.js');
 
-const DB_PATH = './data';
-const BLOCKCHAIN = path.join(DB_PATH, 'blockchain.json');
+const main = async () => {
+  const chain = await new Blockchain('./data');
+  console.log('Blockchain valid:', chain.isValid());
 
-const bc = new Blockchain(BLOCKCHAIN);
-bc.load();
+  const record1 = { value: 13.5, unit: 'm/s', precision: 0.1 };
+  const record2 = { value: 13.6, unit: 'm/s', precision: 0.01 };
+  const record3 = { value: 13.2, unit: 'm/s', precision: 0.001 };
+  const record4 = { value: 13.4, unit: 'm/s', precision: 0.1 };
 
-console.log('Blockchain valid:', bc.isValid());
+  await chain.addBlock(record1);
+  await chain.addBlock(record2);
+  await chain.addBlock(record3);
+  await chain.addBlock(record4);
 
-const record1 = { value: 13.5, unit: 'm/s', precision: 0.1 };
-const record2 = { value: 13.6, unit: 'm/s', precision: 0.01 };
-const record3 = { value: 13.2, unit: 'm/s', precision: 0.001 };
-const record4 = { value: 13.4, unit: 'm/s', precision: 0.1 };
+  console.log('Blockchain:', chain.chain);
+  console.log('Blockchain valid after adding:', chain.isValid());
+};
 
-bc.addBlock(record1);
-bc.addBlock(record2);
-bc.addBlock(record3);
-bc.addBlock(record4);
-
-console.log('Blockchain:', bc.chain);
-console.log('Blockchain valid after adding:', bc.isValid());
+main();
